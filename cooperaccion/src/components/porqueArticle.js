90edb2e93c24a218c6eb55fa09eb5edbf452d818
joaquinSpuches <spuchesjoaquin@gmail.com>
2023-02-09 01:47:33 +0100

    fondo1 fixed

--- cooperaccion/src/components/porqueArticle.js
+++ cooperaccion/src/components/porqueArticle.js
@@ -2,17 +2,17 @@ import React from "react";
 import { useState,useEffect } from "react";
 import {motion} from 'framer-motion'
 export default function PorqueArticle(props) {
-    const x = {opacity:1,  
+    const x = {opacity:1, scale: 1, y:0
         
       }
-    const y = {opacity:0, 
+    const y = {opacity:0, scale: 0, y:-300
       
     }
 
 
     return(
         <motion.div animate={props.clicked ? x : y}    id="porqueArticle" className={`porqueArticle ${props.clicked ? 'porqueArticle-active':''} `}>
-                 <ul style={{height:'100%'}} >
+                 <ul >
                      <li>Porque en un mundo cada vez más desafiante y comple-jo es clave hablar con ellos sobre los valores que reflejan nuestros comportamientos y decisiones que tomamos; poder reflexionar acerca del mundo que queremos crear y cómo desde nuestro accionar somos constructores de ese mundo.</li>
                      <li>Porque el cooperativismo puede ser inspirador para en-contrar ellos mismos formas de solucionar conflictos que viven actualmente: problemas de convivencia en el aula, en la escuela, en su barrio. Visualizar el camino de la cooperación como una alternativa de resolución de conflictos y además como una forma de satisfacer las necesidades de cada uno que beneficia a todos porque no busca “ganar” a costa de los demás.</li>
                      <li>Porque el aprendizaje de los principios y valores del coo-perativismo es un contenido prioritario (está incluido en los NAP -Núcleos de Aprendizaje Prioritarios- definidos por el Consejo Federal de Educación).</li>
