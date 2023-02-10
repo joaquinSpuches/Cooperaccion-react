7149d60c387b78e6896794a331ad8b8b3a4148eb
joaquinSpuches <spuchesjoaquin@gmail.com>
2023-02-10 14:39:53 +0100

    commit

--- cooperaccion/src/components/burgerMenu.js
+++ cooperaccion/src/components/burgerMenu.js
@@ -6,7 +6,7 @@ import React from "react";
 
     
         return(
-            <div style={{paddingLeft:'20px',paddingTop:'10px', position: 'fixed',zIndex:'2',width: '100vw',display: 'flex', backdropFilter: 'blur(4px)' }} > 
+            <div className="bur" style={{paddingLeft:'20px',paddingTop:'10px', position: 'fixed',zIndex:'2',width: '100vw',display: 'flex', backdropFilter: 'blur(4px)' }} > 
             <div onClick={props.handleClick}  className={`hamburger ${props.clicked ? 'is-active':''} `} id="hamburger-6">
             <span className="line"></span>
                 <span className="line"></span>
